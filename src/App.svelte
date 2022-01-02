3917f7833b420fb041359d3045aa141b7bcd8a72
Ali Furkan Demirbaş <furkan.demirbas@usemagnetiq.com>
2022-01-02 16:22:47 +0300

    ts init message change

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
-<script>
-	export let name;
+<script lang="ts">
+	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1><b>Sourc</b> in Progress</h1>
+	<p>Repository Analytics for Enterprise and Open-Source Teams!</p>
 </main>
 
 <style>
@@ -16,7 +16,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #2c8ebe;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
